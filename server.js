444df9484592190239886c4c9804bfa226d4c615
Richard Lake <ralake1985@gmail.com>
2015-05-31 10:19:06 +0100

    fixed ejs typo

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
     include_rts: 'false'
   }, function(error, tweets, response){
     console.log(tweets);
-    res.render('index', {tweets: tweets})
+    res.render('index', {tweets: tweets});
   });
 });
 
